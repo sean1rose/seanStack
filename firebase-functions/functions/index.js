2c84849b25c075c07a7610969531dff86be0c507
Sean Rose <sean1rose@gmail.com>
2019-12-21 08:58:15 +0800

    start set up of authentication

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
-// const serviceAccount = require('../serviceAccount.json');
 const serviceAccount = require('./key/serviceAccount.json')
 
 admin.initializeApp({
@@ -9,53 +9,116 @@ admin.initializeApp({
   databaseURL: "https://seanstack-daf2a.firebaseio.com"
 });
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyBDWmFWHvmBAIdwGrSxEdns2ccj2RMpuco",
+  authDomain: "seanstack-daf2a.firebaseapp.com",
+  databaseURL: "https://seanstack-daf2a.firebaseio.com",
+  projectId: "seanstack-daf2a",
+  storageBucket: "seanstack-daf2a.appspot.com",
+  messagingSenderId: "539122818574",
+  appId: "1:539122818574:web:40560d21196b60a413665f",
+  measurementId: "G-L4VZ350BVR"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore()
 
 app.get('/lists', (req, res) => {
     // for access to db -> admin.firestore() === db
-    admin
-    .firestore()
-    .collection('lists')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then((data) => {
-      let lists = [];
-      data.forEach((doc) => {
-        lists.push({
-          listId: doc.id,
-          user: doc.data().user,
-          createdAt: doc.data().createdAt,
-          list: doc.data().list,
-          title: doc.data.title
-        });
-      })
-      return res.json(lists);
+  db
+  .collection('lists')
+  .orderBy('createdAt', 'desc')
+  .get()
+  .then((data) => {
+    let lists = [];
+    data.forEach((doc) => {
+      lists.push({
+        listId: doc.id,
+        username: doc.data().username,
+        createdAt: doc.data().createdAt,
+        list: doc.data().list,
+        title: doc.data.title
+      });
     })
-    .catch(err => console.log('err', err))
-
+    return res.json(lists);
+  })
+  .catch(err => console.log('err', err))
 })
 
 app.post('/list', (req, res) => {
   const newList = {
     title: req.body.title,
-    user: req.body.user,
+    username: req.body.username,
     createdAt: new Date().toISOString(),
     list: req.body.list
   };
-  admin
-  .firestore()
-    .collection('lists')
-    .add(newList)
-    .then(doc => {
-      res.json({ message: `document ${doc.id} created successfully`})
-    })
-    .catch(err => {
-      res.status(500).json({error: 'something amiss'});
-      console.log('err', err)
-    });
+  db
+  .collection('lists')
+  .add(newList)
+  .then(doc => {
+    res.json({ message: `document ${doc.id} created successfully`})
+  })
+  .catch(err => {
+    res.status(500).json({error: 'something amiss'});
+    console.log('err', err)
+  });
 });
 
-// https://baseurl.com/api/
+// signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    username: req.body.username,
+  }
+
+  let token, userId;
+  db.doc(`/users/${newUser.username}`).get()
+    .then(doc => {
+      // even if doc doesn't exist, will have a snapshot
+      if (doc.exists) {
+        // this username is already taken...
+        return res.status(400).json({ username: `The username ${newUser.username} is already taken`});
+      }
+      else {
+        return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+          userId = data.user.uid;
+          return data.user.getIdToken();
+        })
+        .then(idToken => {
+          token = idToken;
+          // create user document here...
+          const userCredentials = {
+            username: newUser.username,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+          }
+          // save these creds to a doc in our user collection...
+            // give the firebase db the document path (`/users/`)
+            // set creates the document
+          db.doc(`/users/${newUser.username}`).set(userCredentials)
+            .then(() => {
+              return res.status(201).json({ token });
+            })
+        })
+        .catch(err => {
+          console.error(err);
+          if (err.code === 'auth/email-already-in-use') {
+            return res.status(400).json({ email: 'This email is already taken'});
+          }
+          else {
+            return res.status(500).json({ error: err.code });
+          }
+        })
+            
+      }
+    })
+
+});
 
 exports.api = functions.https.onRequest(app);
